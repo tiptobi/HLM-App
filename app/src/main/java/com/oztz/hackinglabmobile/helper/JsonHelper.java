b43161b22b6c195a10ae1e7093df5a31216ed76e
tiptobi <Tobias.Zahner@gmx.ch>
2015-03-22 13:53:49 +0100

    JSON wird korrekt gelesen (momentan noch im JsonHelper implementiert)

--- app/src/main/java/com/oztz/hackinglabmobile/helper/JsonHelper.java
+++ app/src/main/java/com/oztz/hackinglabmobile/helper/JsonHelper.java
@@ -3,89 +3,76 @@ package com.oztz.hackinglabmobile.helper;
 import android.os.AsyncTask;
 import android.util.Log;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.HttpStatus;
-import org.apache.http.StatusLine;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
+import com.google.gson.Gson;
+import com.oztz.hackinglabmobile.businessclasses.Speaker;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLConnection;
 
 /**
  * Created by Tobi on 20.03.2015.
  */
 public class JsonHelper {
 
-    public String readUrl(String urlString) throws Exception {
-        URL url = new URL(urlString);
-        StringBuilder total = new StringBuilder();
-        Log.d("DEBUG", "1");
-        URLConnection urlConnection = url.openConnection();
-        Log.d("DEBUG", "2");
-        try {
-            InputStream in = new BufferedInputStream(urlConnection.getInputStream());
-        } catch (Exception e){
-            Log.d("DEBUG", e.getMessage());
-        }
+    public void getWebsite(String url){
+        new RequestTask().execute(url);
+    }
 
-        Log.d("DEBUG", "3");
-
-        /*try {
-            BufferedReader r = new BufferedReader(new InputStreamReader(in));
-            String line;
-            while ((line = r.readLine()) != null) {
-                total.append(line);
+    private String readUrl(String urlString) throws IOException {
+        InputStream inStream = null;
+        try{
+            URL url = new URL(urlString);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setReadTimeout(7000);
+            con.setConnectTimeout(7000);
+            con.setRequestMethod("GET");
+            con.setDoInput(true);
+            inStream = con.getInputStream();
+            String result = readIt(inStream);
+            Log.d("DEBUG", result);
+            return result;
+        } finally {
+            if(inStream != null){
+                inStream.close();
             }
         }
-        finally {
-            in.close();
-        }*/
-        return total.toString();
+    }
+
+    private String readIt(InputStream stream) throws IOException, UnsupportedEncodingException {
+        StringBuilder inputStringBuilder = new StringBuilder();
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(stream, "UTF-8"));
+        String line = bufferedReader.readLine();
+        while(line != null){
+            inputStringBuilder.append(line);inputStringBuilder.append('\n');
+            line = bufferedReader.readLine();
+        }
+        return inputStringBuilder.toString();
+    }
+
+    private class RequestTask extends AsyncTask<String, String, String> {
+        String result;
+        @Override
+        protected String doInBackground(String... uri) {
+            try {
+                result = readUrl(uri[0]);
+            } catch (Exception e){
+                result = "Site not available";
+            }
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            Speaker[] speakers = new Gson().fromJson(result, Speaker[].class);
+            Log.d("DEBUG", "Name: " + speakers[0].name);
+        }
     }
 }
 
-class RequestTask extends AsyncTask<String, String, String> {
 
-    @Override
-    protected String doInBackground(String... uri) {
-        HttpClient httpclient = new DefaultHttpClient();
-        HttpResponse response;
-        String responseString = null;
-        try {
-            response = httpclient.execute(new HttpGet(uri[0]));
-            StatusLine statusLine = response.getStatusLine();
-            if(statusLine.getStatusCode() == HttpStatus.SC_OK){
-                ByteArrayOutputStream out = new ByteArrayOutputStream();
-                response.getEntity().writeTo(out);
-                responseString = out.toString();
-                out.close();
-            } else{
-                //Closes the connection.
-                response.getEntity().getContent().close();
-                throw new IOException(statusLine.getReasonPhrase());
-            }
-        } catch (ClientProtocolException e) {
-            //TODO Handle problems..
-        } catch (IOException e) {
-            //TODO Handle problems..
-        }
-        return responseString;
-    }
-
-    @Override
-    protected void onPostExecute(String result) {
-        super.onPostExecute(result);
-        //Do anything with response..
-    }
-}
--- app/src/main/java/com/oztz/hackinglabmobile/model/NewsModel.java
+++ app/src/main/java/com/oztz/hackinglabmobile/model/NewsModel.java
@@ -2,10 +2,8 @@ package com.oztz.hackinglabmobile.model;
 
 import android.util.Log;
 
-import com.google.gson.Gson;
 import com.oztz.hackinglabmobile.businessclasses.Speaker;
 import com.oztz.hackinglabmobile.helper.JsonHelper;
-import com.oztz.hackinglabmobile.helper.UrlReader;
 
 /**
  * Created by Tobi on 20.03.2015.
@@ -19,7 +17,11 @@ public class NewsModel{
 
 
     public Speaker[] getSpeaker(){
-        new UrlReader().execute("http://152.96.56.40:8080/hlmng/rest/speaker");
+        try {
+            jhelper.getWebsite("http://152.96.56.40:8080/hlmng/rest/speaker");
+        } catch (Exception e){
+            Log.d("DEBUG", e.getMessage());
+        }
         return null;
     }
 
