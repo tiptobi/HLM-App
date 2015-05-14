6673d298e1a0b641359acfd61d0753a91534de39
tiptobi <Tobias.Zahner@gmx.ch>
2015-05-14 17:14:59 +0200

    Challenge-Detail (mit Video) integriert

---
+++ app/src/main/java/com/oztz/hackinglabmobile/activity/ChallengeDetailActivity.java
@@ -0,0 +1,44 @@
+package com.oztz.hackinglabmobile.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+import com.google.gson.Gson;
+import com.oztz.hackinglabmobile.R;
+import com.oztz.hackinglabmobile.businessclasses.Challenge;
+
+public class ChallengeDetailActivity extends Activity {
+
+    Challenge challenge;
+    VideoView videoView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        challenge = loadChallenge();
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_challenge_detail);
+
+        videoView = (VideoView) findViewById(R.id.challenge_detail_video);
+        setupVideo();
+    }
+
+    private void setupVideo(){
+        if(challenge.aboutchallenge != null && challenge.aboutchallenge.length() > 1){
+            Uri videoUrl = Uri.parse(challenge.aboutchallenge + ".mp4");
+            videoView.setVideoURI(videoUrl);
+            MediaController mc = new MediaController(this);
+            mc.setAnchorView(videoView);
+            videoView.setMediaController(mc);
+            videoView.start();
+        }
+    }
+
+    private Challenge loadChallenge(){
+        Intent intent = getIntent();
+        return new Gson().fromJson(intent.getStringExtra("challenge"), Challenge.class);
+    }
+}
--- app/src/main/java/com/oztz/hackinglabmobile/fragment/ChallengesFragment.java
+++ app/src/main/java/com/oztz/hackinglabmobile/fragment/ChallengesFragment.java
@@ -1,18 +1,21 @@
 package com.oztz.hackinglabmobile.fragment;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import com.google.gson.Gson;
 import com.oztz.hackinglabmobile.MainActivity;
 import com.oztz.hackinglabmobile.R;
+import com.oztz.hackinglabmobile.activity.ChallengeDetailActivity;
 import com.oztz.hackinglabmobile.adapter.ChallengesAdapter;
 import com.oztz.hackinglabmobile.businessclasses.Challenge;
 import com.oztz.hackinglabmobile.helper.App;
@@ -61,10 +64,17 @@ public class ChallengesFragment extends Fragment implements JsonResult {
 
     @Override
     public void onTaskCompleted(String JsonString, String requestCode) {
-        Challenge[] challenges = null;
         try {
-            challenges = new Gson().fromJson(JsonString, Challenge[].class);
+            final Challenge[] challenges = new Gson().fromJson(JsonString, Challenge[].class);
             challengesListView.setAdapter(new ChallengesAdapter(getActivity(), R.layout.item_challenges, challenges));
+            challengesListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView parent, View view, int position, long id) {
+                    Intent intent = new Intent(getActivity(), ChallengeDetailActivity.class);
+                    intent.putExtra("challenge", new Gson().toJson(challenges[position], Challenge.class));
+                    startActivity(intent);
+                }
+            });
         } catch(Exception e){
             Toast.makeText(getActivity(), "Error getting data", Toast.LENGTH_SHORT);
         }
